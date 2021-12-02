67bf26c6e81e2f48c808edb0d9909782a14cacad
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-12-02 10:47:18 -0500

    merge day 1

--- day1.lisp
+++ day1.lisp
@@ -1,4 +1,12 @@
-(load "helpers.lisp")
+(defun readlines (s)
+    (loop for line = (read-line s nil)
+        while line
+        collect line))
+
+(defun take-first-n (collection limit)
+    (let ((diff (- (length collection) limit)))
+        (butlast collection
+            (if (< diff 0) (+ limit diff) diff))))
 
 (defparameter *i* (mapcar #'parse-integer (readlines (open "data/day1.txt"))))
 
--- helpers.lisp
+++
@@ -1,9 +0,0 @@
-(defun readlines (s)
-    (loop for line = (read-line s nil)
-        while line
-        collect line))
-
-(defun take-first-n (collection limit)
-    (let ((diff (- (length collection) limit)))
-        (butlast collection
-            (if (< diff 0) (+ limit diff) diff))))
