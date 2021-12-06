9000f36e63c90801e5dfc3a66f97305b9ded6d28
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-12-05 22:48:31 -0500

    day 5 part 2

--- src/day05/day5.py
+++ src/day05/day5.py
@@ -1,3 +1,10 @@
+import time
+
+# BEWARE
+# THIS IS THE MOST DISGUSTING CODE I"VE WRITTEN IN PROBABLY HALF A DECADE
+# VERY SLOW TOO BUT IT DOES THE JOB
+
+
 def parsePoints(points):
     p1s = points[0].split(",")
     p2s = points[1].split(",")
@@ -5,6 +12,7 @@ def parsePoints(points):
 
 
 data = open("input.txt").read().splitlines()
+#data = open("test.txt").read().splitlines()
 data = list(map(lambda x: x.split(" -> "), data))
 data = list(map(parsePoints, data))
 
@@ -28,26 +36,109 @@ def vecPoints(vector):
     return line
 
 
+def diagonalPoints(vector):
+    p1, p2 = vector
+    line = []
+
+    lowest = p1
+    other = p2
+    if p2[0] < p1[0]:
+        lowest = p2
+        other = p1
+
+    minimum = min([p1[0], p2[0]])
+    maximum = max([p1[0], p2[0]])
+
+    xdir = 1
+    ydir = 1
+
+    if other[0] - lowest[0] < 0:
+        xdir = -1
+
+    if other[1] - lowest[1] < 0:
+        ydir = -1
+
+    for i in range(0, maximum - minimum + 1):
+        line.append((lowest[0] + (i * xdir), lowest[1] + (i * ydir)))
+
+    return line
+
+
 def isHorizVert(vector):
     p1, p2 = vector
     return p1[0] == p2[0] or p1[1] == p2[1]
 
 
-overlap = set()
+def part1():
+    overlap = set()
 
+    for p in data:
+        if not isHorizVert(p):
+            continue
+        line = set(vecPoints(p))
+
+        # compare to other lines
+        for p2 in data:
+            if not isHorizVert(p2):
+                continue
+            line2 = set(vecPoints(p2))
+            if line == line2:
+                continue
+            inter = line.intersection(line2)
+            overlap = overlap.union(inter)
+    return overlap
+
+
+def part2():
+    overlap = set()
+
+    i = 0
+    for p in data:
+        line = None
+        if isHorizVert(p):
+            line = set(vecPoints(p))
+        else:
+            line = set(diagonalPoints(p))
+
+        # compare to other lines
+        j = 0
+        for p2 in data:
+            line2 = None
+            if isHorizVert(p2):
+                line2 = set(vecPoints(p2))
+            else:
+                line2 = set(diagonalPoints(p2))
+
+            if i == j:
+                continue
+            inter = line.intersection(line2)
+            overlap = overlap.union(inter)
+            j += 1
+        i += 1
+    return overlap
+
+
+"""
 for p in data:
     if not isHorizVert(p):
-        continue
-    line = set(vecPoints(p))
+        line = diagonalPoints(p)
+        p1, p2 = p
+        if p1 not in line:
+            print("p1", p1, "not in line!", p1,
+                  p2, line, "|", line[0], line[-1])
+        if p2 not in line:
+            print("p2", p2, "not in line!", p1, p2,
+                  line, "|",  line[0], line[-1])
+"""
+start = time.time()
 
-    # compare to other lines
-    for p2 in data:
-        if not isHorizVert(p2):
-            continue
-        line2 = set(vecPoints(p2))
-        if line == line2:
-            continue
-        inter = line.intersection(line2)
-        overlap = overlap.union(inter)
+print(len(list(part1())))
 
-print(len(list(overlap)))
+t1 = time.time()
+print("Part 1 done in ", (t1 - start))
+
+print(len(list(part2())))
+
+t2 = time.time()
+print("Part 2 done in ", (t2 - t1))
+print("Total done in", (t2 - start))
