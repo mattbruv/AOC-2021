1af3e4aedc821e7c8f80d510e734c397b619e4a8
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-12-08 10:04:43 -0500

    day8

--- src/day08/day8.py
+++ src/day08/day8.py
@@ -9,4 +9,16 @@ data = list(map(lambda x: x.split(" | "), open(
 
 data = list(map(parseLine, data))
 
+
+def decodeLine(line):
+    print(line)
+
+
+a = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"
+b = "cdfeb fcadb cdfeb cdbaf"
+
+data = (a.split(), b.split())
 print(data)
+
+# for d in data:
+#    print(decodeLine(d))
