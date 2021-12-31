6bfc1fc644c2ace1fcd066a04dfc6cde6126b9da
Matt <5638426+mattbruv@users.noreply.github.com>
2021-12-31 00:52:51 -0500

    2015 day 4

--- src/2015/day04/day04.py
+++ src/2015/day04/day04.py
@@ -1,3 +1,17 @@
+import hashlib
+
 input = open("input.txt").read().strip()
 
-print("2015 day 4")
+
+def findHash(zeroes):
+    i = 0
+    while True:
+        s = input + str(i)
+        out = hashlib.md5(s.encode()).hexdigest()
+        if out[:zeroes] == "0" * zeroes:
+            return i
+        i += 1
+
+
+print(findHash(5))
+print(findHash(6))
