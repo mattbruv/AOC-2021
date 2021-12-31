ddc7f12a1844e4e4103e9d2acf2795cd4313dd9f
Matt <5638426+mattbruv@users.noreply.github.com>
2021-12-31 02:36:27 -0500

    2015 day 17

--- src/2015/day17/day17.py
+++ src/2015/day17/day17.py
@@ -1,3 +1,23 @@
-input = open("input.txt").read().strip()
+from itertools import combinations
 
-print("2015 day 17")
+input = list(map(int, open("input.txt").read().strip().splitlines()))
+
+count = 0
+
+comboSet = set()
+for i in range(2, len(input)):
+    combos = combinations(input, r=i)
+    for c in combos:
+        if sum(c) == 150:
+            comboSet.add(c)
+            count += 1
+
+print(count)
+
+minContainers = len(sorted(comboSet, key=len)[0])
+
+print(
+    len(
+        list(filter(lambda x: x == 150, map(sum, combinations(input, r=minContainers))))
+    )
+)
