91d05b3e5e528f7c9f7187f948f355bff3ad06e9
Matt <5638426+mattbruv@users.noreply.github.com>
2021-12-31 02:46:11 -0500

    generate list of shortest input problems

--- gen.py
+++ gen.py
@@ -7,7 +7,7 @@ from requests.sessions import session
 sessionCookie = ""
 inputUrl = "https://adventofcode.com/{year}/day/{day}/input"
 
-years = range(2015, 2021)
+years = range(2015, 2022)
 # years = range(2021, 2022)
 
 
@@ -15,19 +15,21 @@ def pad(day):
     return "{:02d}".format(day)
 
 
+ins = []
+
 for y in years:
     for day in range(1, 26):
         dayString = "day" + str(pad(day))
         path = "./src/" + str(y) + "/" + dayString + "/"
         reqUrl = inputUrl.format(year=y, day=day)
-        # data = requests.get(reqUrl, cookies={"session": sessionCookie})
-        # problemInput = data.text.strip()
-        print(path)
-        # data = open(path + "input.txt").read().strip()
-        data = """input = open("input.txt").read().strip()
-
-print("{year} day {day}")""".format(
-            year=y, day=day
+        i = path + "input.txt"
+        ins.append(
+            (
+                str(y) + " " + str(day),
+                len(open(i).read()),
+                len(open(i).read().splitlines()),
+            )
         )
-        open(path + "day" + pad(day) + ".py", "w").write(data)
-        # time.sleep(0.5)
+
+out = sorted(ins, key=lambda x: x[1])
+open("input-len.txt", "w").write("\n".join(list(map(str, out))))
