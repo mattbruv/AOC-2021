738cdf72063a3e39f8037c5067477b35d30b071e
Matt <5638426+mattbruv@users.noreply.github.com>
2021-12-31 00:29:34 -0500

    2015 day 2

--- src/2015/day02/day02.py
+++ src/2015/day02/day02.py
@@ -1,3 +1,26 @@
-input = open("input.txt").read().strip()
+input = open("input.txt").read().strip().splitlines()
 
-print("2015 day 2")
+
+def parse(line):
+    return tuple(map(int, line.split("x")))
+
+
+def area(tup):
+    l, w, h = tup
+    a = l * w
+    b = w * h
+    c = h * l
+    return 2 * a + 2 * b + 2 * c + min([a, b, c])
+
+
+def ribbon(tup):
+    l, w, h = tup
+    smallest = tuple(sorted(list(tup))[:2])
+    a, b = smallest
+    return (l * w * h) + (a * 2 + b * 2)
+
+
+input = list(map(parse, input))
+
+print(sum(map(area, input)))
+print(sum(map(ribbon, input)))
