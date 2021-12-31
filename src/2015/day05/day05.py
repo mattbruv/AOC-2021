870baca1c5a70104ba8b717e39ee7ca1f44e057a
Matt <5638426+mattbruv@users.noreply.github.com>
2021-12-31 02:01:51 -0500

    2015 day 5

--- src/2015/day05/day05.py
+++ src/2015/day05/day05.py
@@ -33,6 +33,7 @@ def isNice2(string):
     for i in range(0, len(string)):
         partial = string[i : i + 3]
         if len(partial) == 3:
+            # print(string, partial, string[0:i], string[i : i + 3], string[i + 3 : :])
             a, b, c = tuple(partial)
             if a == c:
                 rule2 = True
@@ -40,24 +41,29 @@ def isNice2(string):
     rule1 = False
     prev = ""
     counts = {}
+    lastIdx = 0
 
     for i in range(0, len(string)):
         nextPair = string[i : i + 2]
 
         if nextPair == prev:
-            continue
+            if lastIdx == i - 1:
+                continue
         prev = nextPair
+        lastIdx = i
 
-        if nextPair not in counts:
-            counts[nextPair] = 1
-        else:
-            counts[nextPair] += 1
-        count = counts[nextPair]
-        if count == 2:
-            rule1 = True
-            break
+        if len(nextPair) == 2:
+            # print(string, nextPair, string[0:i], string[i : i + 2], string[i + 2 : :])
+            if nextPair not in counts:
+                counts[nextPair] = 1
+            else:
+                counts[nextPair] += 1
+            count = counts[nextPair]
+            if count == 2:
+                rule1 = True
+                break
 
-    return (rule1 != False) and (rule2 != False)
+    return (rule1 == True) and (rule2 == True)
 
 
 tests = [
@@ -65,11 +71,14 @@ tests = [
     ("xxyxx", True),
     ("uurcxstgmygtbstg", False),
     ("ieodomkazucvgmuy", False),
+    ("bkkkkcwegvypbrio", True),
 ]
 
 print(len(list(filter(isNice, input))))
 print(len(list(filter(isNice2, input))))
 
+"""
 for t in tests:
     s, res = t
-    print("test:", s, "pass?", isNice2(s) == res)
+    print("test:", s, "passes test?", isNice2(s) == res)
+"""
