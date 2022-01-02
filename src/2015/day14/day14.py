a5f03b59ac829fc3f879edc2815eee06aad6e43e
Matt <5638426+mattbruv@users.noreply.github.com>
2022-01-02 13:55:57 -0500

    2015 day 14

--- src/2015/day14/day14.py
+++ src/2015/day14/day14.py
@@ -24,5 +24,18 @@ def distance(deer, seconds):
     return total * speed * goTime - adjust
 
 
-
 print(max(list(map(lambda x: distance(x, 2503), input))))
+
+scores = {}
+
+for i in input:
+    scores[i] = 0
+
+for seconds in range(0, 2504):
+    deerScores = list(map(lambda x: (x, distance(x, seconds)), input))
+    best = max([x[1] for x in deerScores])
+    deers = [x[0] for x in deerScores if x[1] == best]
+    for d in deers:
+        scores[d] += 1
+
+print(max([scores[x] for x in scores]))
