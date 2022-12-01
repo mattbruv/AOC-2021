48b9917bbae539d7cb5462fc86f104882336bd72
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-12-01 04:28:54 -0500

    non-working stopping poitn

--- src/2022/day01/day01.s
+++ src/2022/day01/day01.s
@@ -3,15 +3,49 @@
 
 .global part1
 part1:
+    movl $0, %ebx # highestSoFar
+    movl $0, %ecx # counter
+
+.ResetAndAdd:
+    movl $0, %edx # accumulator
+
+.AddNext:
     leaq nums(%rip), %rax
-    addq $4,  %rax 
+    addq %rcx,  %rax 
     movl (%rax), %eax
-    movl $3259, %eax
+    addl %eax, %edx
+    addl $4, %ecx
+
+    cmpl $-1, %eax
+    je .NextElf
+    jmp .AddNext
+
+.NextElf:
+    # iif we have looked through all the numbers, return
+    cmpl (4 * 2237), %ecx
+    jge .Finish
+
+    # check if new sum is > ebx
+    cmpl %edx, %ebx
+    jle .ResetAndAdd
+
+    # it's greater than
+    movl %edx, %ebx
+
+
+    # highest = EBX
+    # accumulator
+
+    # iif n = -1, accumulator > highest? highest = acc
+    # return highest
+
+.Finish:
+    movl %ebx, %eax
     ret
 
 .global part2
 part2:
-    movl $42069, %eax
+    movl $0, %eax
     ret
 
 
