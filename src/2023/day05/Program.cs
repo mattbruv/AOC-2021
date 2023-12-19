59bf459a5787ed9217b50f5d12c82236849a4e6c
Matt Pitts <matt.pitts@nuqleous.com>
2023-12-19 14:22:11 -0600

    adsjfpaoisdjfa

--- src/2023/day05/Program.cs
+++ src/2023/day05/Program.cs
@@ -1,2 +1,18 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllText("../../../input.txt").Split("\n\n");
+
+
+var seeds = lines.First().Split(":")[1].Trim().Split().Select(x => Int64.Parse(x)).ToList();
+
+var maps = lines.Skip(1)
+    .Select(x => new Mapping(x))
+    .ToList();
+
+Console.WriteLine(seeds);
+
+record Mapping(string map)
+{
+    private string[] Lines => map.Split("\n");
+
+    public string From => Lines.First().Split("-")[0];
+    public string To => Lines.First().Split("-")[2].Split().First();
+}
