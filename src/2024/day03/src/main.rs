722b4d38304907cde4e9267c82f41aa9b6b48599
Matt P <mattp3259@gmail.com>
2024-12-03 07:56:38 +0100

    part 1

--- src/2024/day03/src/main.rs
+++ src/2024/day03/src/main.rs
@@ -1,3 +1,14 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut muls: Vec<(i32, i32)> = vec![];
+
+    for (_, [a, b]) in regex.captures_iter(input).map(|c| c.extract()) {
+        muls.push((a.parse().unwrap(), b.parse().unwrap()));
+    }
+
+    let part1: i32 = muls.iter().map(|(a, b)| *a * *b).sum();
+    println!("{:?}", part1);
 }
